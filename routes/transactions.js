88f44d4f1738a0721e919c1d484803c9bb61decb
Mili <muhamed.mili92@gmail.com>
2025-02-11 02:12:04 +0100

    transactions

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fetch = require('node-fetch');
-
+const pool = require("../db/db");
 const router = express.Router();
 
 const apiKey = "B1MY6SAR5HS9D5HQUVA5T1RHMQDXTRVHSS";
@@ -23,4 +23,32 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/transac', async (req, res) => {
+    const { hash, from, to, value, items, date } = req.body;
+
+    try {
+        await pool.query(`
+            INSERT INTO transactions (hash, "from_address", "to_address", value, items, date)
+            VALUES ($1, $2, $3, $4, $5, $6)
+        `, [hash, from, to, value, items, date]);
+
+        res.status(201).json({ message: 'Transaction saved successfully' });
+    } catch (error) {
+        console.error('Error saving transaction:', error.message);
+        res.status(500).json({ error: 'Failed to save transaction' });
+    }
+});
+router.get('/filtered', async (req, res) => {
+    try {
+        const result = await pool.query(` -- Assure-toi que "db.query" est correctement configuré
+            SELECT *
+            FROM transactions
+            ORDER BY date DESC
+        `);
+        res.status(200).json(result.rows); // Retourne toutes les transactions triées par date
+    } catch (error) {
+        console.error('Error fetching transactions:', error.message);
+        res.status(500).json({ error: 'Failed to fetch transactions' });
+    }
+});
 module.exports = router;
